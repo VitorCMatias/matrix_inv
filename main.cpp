6b5b4bd0079cfdbfed489813b67257b95be4140a
Vitor <vitorcosta.matias@usp.br>
2022-07-07 01:37:40 -0300

    main updates

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@
 int main() {
 
     Matrix A(3);
-    for (int i = 0; i < 100; ++i) {
+    A.inicializar_matriz_teste();
+    printf("(MATRIX)");
+    A.print(A.get_matrix());
+    A.invert();
+    printf("(INVERSE)");
+    A.print(A.get_inverse());
+
+    /*for (int i = 0; i < 100; ++i) {
         A.set_random(1, 99);
         printf("(MATRIX)");
         A.print(A.get_matrix());
         A.invert();
         printf("(INVERSE)");
         A.print(A.get_inverse());
-    }
+    }*/
 
 
     A.erase();
